3e021977b10d3605c163af6285801a61f21452fc
VagnerSilva <vagner23silva@gmail.com>
2019-10-17 19:27:59 -0300

    Flutter - Timer

---
+++ lib/screens/timer/timer.state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:timer/screens/timer/timer.stateful.dart';
+
+class TimerState extends State<TimerStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
